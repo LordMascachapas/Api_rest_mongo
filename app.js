9d3a6e66436a1d60e1e26af39365da251e4a42bf
LordMascachapas <32301199+LordMascachapas@users.noreply.github.com>
2018-11-22 15:59:12 +0100

    Access-Control-Allow-Origin header

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ const api = require('./routes/index')
 
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
+app.use((req, res, next) => {
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	next();
+})
 app.use('/api', api)
 
 module.exports = app
